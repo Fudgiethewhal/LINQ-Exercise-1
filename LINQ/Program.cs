b0437893cca457df21dddb92442dc4c2500129f4
Bryseida Hernandez <bryseida.hernandez@yahoo.com>
2025-02-02 13:47:43 -0800

    finished exercise

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var gameNames = new List<string>()
+            {
+                "Fallout: New Vegas",
+                "Call of Duty",
+                "Diablo",
+                "Assassin's Creed",
+                "Phasmaphobia",
+                "Doom",
+                "Mario"
+            };
+            
+            var orderedGameNames = gameNames.OrderBy(gameName => gameName.Length).ToList(); 
+            orderedGameNames.ForEach(game => Console.WriteLine(game));
         }
     }
 }
